ba8eb580de0914e62766d5229c3e10b6714b428e
Duc Hoang Nguyen <lazycoder97@gmail.com>
2016-09-26 01:32:28 +0700

    Add templates and sublime-completions

---
+++ [Template] Duc.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define llong long long
+#define long llong
+
+int n;
+
+void enter()
+{
+}
+
+void solve()
+{
+}
+
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+	if (ifstream("test.inp")) cin.rdbuf((new ifstream("test.inp"))->rdbuf());
+
+	enter();
+	solve();
+}
