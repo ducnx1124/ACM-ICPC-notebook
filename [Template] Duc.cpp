87022dca43b9cac8b559a27518a02d256f3b714d
Duc Hoang Nguyen <lazycoder97@gmail.com>
2016-10-01 18:42:34 +0700

    Thêm pbds vào template

--- [Template] Duc.cpp
+++ [Template] Duc.cpp
@@ -1,5 +1,9 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 using namespace std;
+using namespace __gnu_pbds;
+#define sorted_array tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
 #define llong long long
 #define long llong
 
