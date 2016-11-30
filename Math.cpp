4c11a9de88576a9c81b3543ed0b14c8b9f227077
lazyc97 <lazycoder97@gmail.com>
2016-11-30 17:28:04 +0700

    convex hull trick + module invert

--- Math.cpp
+++ Math.cpp
@@ -11,7 +11,18 @@ struct point
 // = 0 -> parallel
 // > 0 -> left
 // < 0 -> right
-inline int ccw(point a, point b, point c)
+inline long ccw(point a, point b, point c)
 {
 	return (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x);
+}
+
+long euclid_extended(int nr, int r, int nt, int t)
+{
+	if (nr == 0) return t;
+	return euclid_extended(r - r / nr * nr, nr, t - r / nr * nt, nt);
+}
+
+long inverse_mod(int x, int module)
+{
+	return euclid_extended(x, module, 1, 0);
 }
