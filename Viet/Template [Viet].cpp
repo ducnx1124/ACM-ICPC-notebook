373dea681aa8dfdb1d90031b6b432422e970e142
lazyc97 <lazycoder97@gmail.com>
2016-11-30 23:04:32 +0700

    Upload docx

--- Viet/Template [Viet].cpp
+++ Viet/Template [Viet].cpp
@@ -1,12 +1,12 @@
 #include <bits/stdc++.h>
-#define N 101
+#define N
 #define INF
 #define rep(i, a, b) for(int i = a; i <= b; i++)
 #define per(i, b, a) for(int i = b; i >= a; i--)
 #define bitcount(S) __builtin_popcount(S)
 #define ll (long long)
 #define db (double)
-#define dbg(x) std::cout << x << std::endl
+#define dbg(x) std::cout << #x << " " << x << std::endl
 #define sz(x) x.size()
 #define fi first
 #define se second
